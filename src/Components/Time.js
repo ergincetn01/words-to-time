6188a5c2c46b0733d4ed9c89061d4672edced74d
ergincetn01 <ergincetn01@gmail.com>
2023-07-05 01:13:05 +0300

    word count added

--- src/Components/Time.js
+++ src/Components/Time.js
@@ -1,10 +1,24 @@
 function Time() {
+    const countWords=(txt)=> {
+        
+        
+    } 
 
+    function getWordCount(str) {
+        return str.split(' ')
+          .filter(function(n) { return n != '' })
+          .length;
+   }
+
+    const estTime=()=> {
+
+    }
+    var st = "fgfdg fgh ghtg ghtrhret ghthth gbrehg"
     return (
         <div>
             <p> Time estimated:
             </p>
-            <p>total words: </p>
+            <p>total words: {getWordCount(st)} </p>
 
         </div>
     )
